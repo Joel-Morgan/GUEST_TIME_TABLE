4f572b47245fd479fceed1c9ef34193491280b44
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-03-09 15:04:52 +0000

    lolBadPractice

--- pullrequests.py
+++ pullrequests.py
@@ -5,10 +5,9 @@ g = Github("4b1fd50ddd571e8bba0ec82937ff5b61f939f3d7")
 for repo in g.get_user().get_repos():
     print(repo.name)
   
-repo = g.get_user().get_repos("GUEST_TIME_TABLE")
-
+repo = g.get_repo("Pounder206/GUEST_TIME_TABLE")
 open_issues = repo.get_issues(state='open')
-for issue in open_issues:
-  print(issues)
+for i in open_issues:
+  print(i)
 # title = name of lecture
 # body will be lecture name and time  
