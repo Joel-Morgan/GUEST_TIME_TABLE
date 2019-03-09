e5104d6b25be0070e38b8451bef7450fdfe301c3
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-03-09 16:07:22 +0000

    v1

--- pullrequests.py
+++ pullrequests.py
@@ -1,13 +1,14 @@
 from github import Github
 
-g = Github("a5a05a98741be468e722d7658e151cb70af0e373")
+g = Github("698ea9b37150a93463c3f2834021a20a8bb84654")
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
+#for repo in g.get_user().get_repos():
+#    print(repo.name)
   
 repo = g.get_repo("Pounder206/GUEST_TIME_TABLE")
 open_issues = repo.get_issues(state='open')
 for i in open_issues:
-  print(i.body)
+  print(i.title)
+  print((i.body).split())
 # title = name of lecture
 # body will be lecture name and time  
