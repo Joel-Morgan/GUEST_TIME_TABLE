44f54cfb809a2343d7a4b91ff02aadcde4036400
GCHeroes1 <rajeshgoyal@hotmail.com>
2019-03-09 14:00:44 +0000

    NEW BEGINNINGS

--- src/Spaghetti.java
+++ src/Spaghetti.java
@@ -1,3 +1,13 @@
 public class Spaghetti {
     String requestURL =
 }
+// the room booking in a format they can read ;-)
+// the way the user is gonna submit, is with an issue request ;-(
+// title is the lecturer name
+// the body is the room booking
+// make a http request to github asking for the issue
+// its very nice, returns a json format string :/
+// where you can index the body and title directly :o
+// after that you gonna use those two pieces of data to make a http request for the API for the full details of the booking
+// and then, just gonna, store that in a json format file and push it to the github
+// agree?
